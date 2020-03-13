df965c8444f0a6fdf718e095738bf0bddd371bf9
Line Båserud <lineb@pc5240.pc.met.no>
2020-03-13 16:58:10 +0100

    updated distrCol.R to handle NAs

--- functions/distrCol.R
+++ functions/distrCol.R
@@ -14,16 +14,19 @@ distrCol <- function(data,anomaly_shift=NA){
   if (!is.na(anomaly_shift)){data$val <- data$val-anomaly_shift}
   vcol=data.frame()
   for (n in 1:length(data$val)){
-          if(data$val[n]<0){
+          if(!is.na(data$val[n]) && data$val[n]<0){
             tmpcol<-data.frame(col="blue")
             vcol <- rbind(vcol,tmpcol)
-       } else if (data$val[n]==0){
+       } else if (!is.na(data$val[n]) && data$val[n]==0){
            tmpcol<-data.frame(col="black")
            vcol <- rbind(vcol,tmpcol)
-       } else if (data$val[n]>0){
+       } else if (!is.na(data$val[n]) && data$val[n]>0){
            tmpcol<-data.frame(col="red")
            vcol <- rbind(vcol,tmpcol)
-       }
+       } else if (is.na(data$val[n])){
+           tmpcol<-data.frame(col=NA)
+           vcol <- rbind(vcol,tmpcol)
+       } 
   }
   return(vcol)
 }
