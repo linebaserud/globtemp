e06d6f64afddd644039ea4bb8f7be9ba24140afd
Line Båserud <lineb@pc5240.pc.met.no>
2020-03-20 20:12:57 +0100

    add adapable legend text for compare-mode, add adaptable x-axis scaling

--- globtemp.r
+++ globtemp.r
@@ -31,22 +31,25 @@ datasets <- c('NASA','Copernicus','HadCRUT') # dataset(s) to plot, e.g. 'NASA', 
 
 refs<-1981            # start reference period
 refe<-2010            # end reference period
-period <- 'Yearly'    # type of data, e.g. 'Yearly', 'Jan', or 'Feb', ...
+period <- 'Jan'    # type of data, e.g. 'Yearly', 'Jan', or 'Feb', ...
 
 orig<-F               # T/F plot data relative to original reference period
 
 # get data ----------------------------------------------------------------------------------
+
+y1 <- data.frame()
 
 # NASA/GISS
 if (!is.na(match('NASA',datasets))){
   D <- readNASA(filenameNASA,period) 
   refsNASA <- 1951
   refeNASA <- 1980
-  year1 <- 1880
+  y1NASA <- 1880
   if (refsNASA == refs & refeNASA == refe){m_new <- 0} # if choosen ref period is equal to original in dataset
   if (refsNASA != refs | refeNASA != refe){m_new <- anom2anom(D,refs,refe)} # else change ref period
   textNASA <- "NASA/GISS GHCN-v4 1880-12/2019 + SST: ERSST v5 1880-12/2019 (original reference period: 1951-1980)"
   D <- cbind(D,distrCol(D,m_new)) # add colomn with colors for plotting
+  y1 <- rbind(y1,y1NASA)
 }
 
 # Copernicus
@@ -54,13 +57,14 @@ if (!is.na(match('Copernicus',datasets))){
   D2 <- readCopernicus(filenameCop,period)
   refsCop <- 1981
   refeCop <- 2010
-  year1 <- 1979 
+  y1Cop <- 1979 
   textCopernicus <- "ERA5 Copernicus Climate Change Service/ECMWF (original reference period: 1981-2010)"
   if (D2$y[1] > refs){textCopernicus <- "**Warning**: NO Copernicus data for choosen reference period"} # add before and do not plot
   colnames(D2) <- c("y", "val")
   if (refsCop == refs & refeCop == refe){m2_new <- 0}                       # if ref period equal to original...
   if (refsCop != refs | refeCop != refe){m2_new <- anom2anom(D2,refs,refe)} # ...else change ref period
   D2 <- cbind(D2,distrCol(D2,m2_new))                                       # add colomn with colors for plotting
+  y1 <- rbind(y1,y1Cop)
 }
 
 # HadCRUT4
@@ -69,12 +73,15 @@ if (!is.na(match('HadCRUT',datasets))){
   if (period == 'Yearly'){D3 <- readHadCRUT(filenameHadYearly,period)}
   refsHad <- 1961
   refeHad <- 1990
-  year1 <- 1850
+  y1Had <- 1850
   textHadCRUT <- "HadCRUT4: CRUTEM4 surface air temperature + HadSST3 sea-surface temperature (original reference period: 1961-1990)"
   if (refsHad == refs & refeHad == refe){m3_new <- 0}                       # if chosen ref period equal to original...
   if (refsHad != refs | refeHad != refe){m3_new <- anom2anom(D3,refs,refe)} # ...else change ref period
   D3 <- cbind(D3,distrCol(D3,m3_new))                                       # add colomn with colors for plotting
+  y1 <- rbind(y1,y1Had)
 }
+
+y1 <- min(y1) # scaling for x-axis
 
 # plot data ----------------------------------------------------------------------------------
 
@@ -92,8 +99,10 @@ if (!is.na(match('NASA',datasets)) & length(datasets)==1){
     geom_point(aes(y=D$val-m_new,x=D$y),pch=16,size=4,color=D$col)+
     if (orig){geom_line(aes(y=D$val,x=D$y),size=1,linetype="solid",color="grey")} # relative to  original reference period
 }
-if (!is.na(match('NASA',datasets)) & length(datasets)>1){p=p+geom_line(aes(y=D$val-m_new,x=D$y),size=1.5,linetype="solid",color="pink")}
-
+if (!is.na(match('NASA',datasets)) & length(datasets)>1){
+  p=p+geom_line(aes(y=D$val-m_new,x=D$y),size=1.5,linetype="solid",color="orange")
+  p=p+annotate("text",x=y1+(2020-y1)/2-25,y=0.95,label="NASA/GISS",color="orange",size=6)  
+}
 
 # Copernicus
 if (!is.na(match('Copernicus',datasets)) & length(datasets)==1  & refs > D2$y[1]){
@@ -104,7 +113,10 @@ if (!is.na(match('Copernicus',datasets)) & length(datasets)==1  & refs > D2$y[1]
     if (orig){geom_line(aes(y=D2$val,x=D2$y),size=1,linetype="solid",color="grey")} # relative to  original reference period
 }
 if (refs < D2$y[1]){p=p+labs(subtitle=paste(textCopernicus))}                       # print warning
-if (!is.na(match('Copernicus',datasets)) & length(datasets)>1){p=p+geom_line(aes(y=D2$val-m2_new,x=D2$y),size=1.5,linetype="solid",color="orange")}
+if (!is.na(match('Copernicus',datasets)) & length(datasets)>1){
+  p=p+geom_line(aes(y=D2$val-m2_new,x=D2$y),size=1.5,linetype="solid",color="red")
+  p=p+annotate("text",x=y1+(2020-y1)/2-5,y=0.95,label="Copernicus",color="red",size=6)  
+}
 
 # HadCRUT
 if (!is.na(match('HadCRUT',datasets)) & length(datasets)==1){
@@ -114,7 +126,10 @@ if (!is.na(match('HadCRUT',datasets)) & length(datasets)==1){
     geom_point(aes(y=D3$val-m3_new,x=D3$y),pch=16,size=4,color=D3$col)
     if (orig){geom_line(aes(y=D3$val,x=D3$y),size=1,linetype="solid",color="grey")} # relative to original reference period
 }
-if (!is.na(match('HadCRUT',datasets)) & length(datasets)>1){p=p+geom_line(aes(y=D3$val-m3_new,x=D3$y),size=1.5,linetype="solid",color="brown")}
+if (!is.na(match('HadCRUT',datasets)) & length(datasets)>1){
+  p=p+geom_line(aes(y=D3$val-m3_new,x=D3$y),size=1.5,linetype="solid",color="brown")
+  p=p+annotate("text",x=y1+(2020-y1)/2+15,y=0.95,label="HadCRUT4",color="brown",size=6)  
+}
 
 p=p+theme(
       axis.text.x=element_text(size=20,angle = 90,vjust=0.5)
@@ -125,13 +140,12 @@ p=p+theme(
     , panel.grid.minor = element_blank() 
     , legend.background = element_rect(fill = "transparent") 
     , legend.box.background = element_rect(fill = "transparent")
-    , plot.title = element_text(hjust = 0.5)
+    , plot.title = element_text(hjust = 0.5,size=18)
     , plot.subtitle = element_text(hjust = 0.5))
-  
 
-p=p + scale_x_continuous(limits=c(year1-10,2020),breaks=seq(year1-10,2020,10))+
+p=p + scale_x_continuous(limits=c(y1-10,2020),breaks=seq(y1-10,2020,10))+
       scale_y_discrete(limits=c(-1,-0.5,0,0.5,1,1.5),expand = c(0.1,0.15))
 
 print(p)
-#ggsave(plot=p,file="plots/global_NASA_09032020.png", bg = "transparent",width = 11, height = 6) # save figure
+#ggsave(plot=p,file="example_compare.png", bg = "transparent",width = 11, height = 6) # save figure
 
