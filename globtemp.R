ebf97cb2e9781f963fca2025836a84830c7160b8
Line Båserud <lineb@pc5240.pc.met.no>
2020-11-10 20:43:33 +0100

    added option plotting type

--- globtemp.R
+++ globtemp.R
@@ -24,7 +24,7 @@ for (fun in fun_list) {source(paste0("functions/", fun))}
 
 # main function -------------------------------------------------------------------------------
 
-globtemp <- function(datasets, refs, refe, period, save_option, save_name){
+globtemp <- function(datasets, refs, refe, period, plot_type, save_option, save_name){
 
   # get data ----------------------------------------------------------------------------------
   
@@ -100,49 +100,46 @@ globtemp <- function(datasets, refs, refe, period, save_option, save_name){
   max_all <- max(max_all)   # adaptive scaling y-axis max
   min_all <- min(min_all)   # adaptive scaling y-axis min
 
-
   # functions for plotting ----------------------------------------------------------------------
 
   top5 <- function(period, df_val, df_y, new_mean, df_name){
-    cat(paste0("---- ", period, " top 5 ", df_name, " ----\n "))
+    cat(paste0("---- ", period, " top 5 ", df_name, " (relative to ", refs, "-", refe, ") ----\n "))
     cat(paste0(c(1:5), ": ", format(round(rev(tail(df_val[order(df_val)], n = 5)) - new_mean, 2), nsmall = 2), " (", rev(tail(df_y[order(df_val)], n = 5)), ")\n")) 
   }
 
-  plot_single_points <- function(p, df_val, df_y, df_col, new_mean, df_name) {
+  plot_singular_points <- function(p, df_val, df_y, df_col, new_mean, df_name, last_data_point) {
     p <- p +
-         labs(subtitle = paste(df_name)) +
+         labs(subtitle = paste0(df_name,"\n (last data ", last_data_point, ")")) +
          geom_line(aes(y = df_val - new_mean, x = df_y), size = 1, linetype = "solid", color = "black") +
          geom_point(aes(y = df_val - new_mean, x = df_y), pch = 16, size = 4, color = df_col)
     return(p)
   }
 
-  plot_single_bars <- function(p, df_val, df_y, df_col, new_mean, df_name) {
+  plot_singular_bars <- function(p, df_val, df_y, df_col, new_mean, df_name, last_data_point) {
     p <- p +
-         labs(subtitle = paste(df_name)) +
+         labs(subtitle = paste0(df_name,"\n (last data ", last_data_point, ")")) +
          geom_col(aes(y = df_val - new_mean, x = df_y), width = 0.8, fill = df_col)
     return(p)
   }
 
-  plot_plural <- function(p, df_val, df_y, new_mean, line_color, df_name, last_data_point, text_pos_offset) {
+  plot_plural_lines <- function(p, df_val, df_y, new_mean, line_color, df_name, last_data_point, text_pos_offset) {
     p <- p +
          geom_line(aes(y = df_val - new_mean, x = df_y), size = 1.5, linetype = "solid", color = line_color, na.rm = TRUE) +
-         annotate("text", x = y1 + (year_max - y1)/2 - text_pos_offset, y = 0.95, label = df_name, color = line_color, size = 6) +
-         annotate("text", x = y1 + (year_max - y1)/2 - text_pos_offset, y = 0.85, label = paste0("(", last_data_point, ")"), color = line_color, size = 4)
+         annotate("text", x = y1 + (year_max - y1)/2 - text_pos_offset, y = (ceiling(max_all / 0.5) * 0.5) - 0.05, 
+                          label = df_name, color = line_color, size = 6) +
+         annotate("text", x = y1 + (year_max - y1)/2 - text_pos_offset, y = (ceiling(max_all / 0.5) * 0.5) - 0.15, 
+                          label = paste0("(last data ", last_data_point, ")"), color = line_color, size = 4)
     return(p)
   }
 
   # plot data ----------------------------------------------------------------------------------
 
-  cat("-----------------------------------------------------------------------------------------------------------------------------\n")
-  print(paste0("Top 5 (relative to ", refs, "-", refe, "):")) 
-  cat("\n") 
+  cat("-----------------------------------------------------------------------------------------------------------------------------\n\n")
 
   p <- ggplot() +
        xlab("") +
        ylab("") +
        geom_hline(yintercept = 0, color = "grey") +
-       geom_vline(xintercept = 1880, color = "grey") +
-       geom_vline(xintercept = 1990, color = "grey") +
        geom_vline(xintercept = 2020, color = "grey") +
        geom_segment(aes(x = refs, xend = refe, y = 0, yend = 0), color = "grey60", size = 3) + 
        labs(title = paste(period, "global temperature anomalies (°C)  relative to", refs, "-", refe))
@@ -150,13 +147,14 @@ globtemp <- function(datasets, refs, refe, period, save_option, save_name){
   # NASA
   # plot as single dataset
   if (!is.na(match('NASA', datasets)) & length(datasets) == 1) {
-    p <- plot_single_bars(p, D$val, D$y, D$col, m_new, textNASA) 
-  # p <- plot_single_points(p, D$val, D$y, D$col, m_new, textNASA) 
+    if (plot_type == 'points') p <- plot_singular_points(p, D$val, D$y, D$col, m_new, textNASA, out[2]) 
+    if (plot_type == 'bars')   p <- plot_singular_bars(p, D$val, D$y, D$col, m_new, textNASA, out[2])
+    if (plot_type != 'bars' & plot_type != 'points') cat(paste0('** WARNING **: set plot_type to "points" or "bars" \n\n'))
     top5(period, D$val, D$y, m_new, textNASA_short)
   }
   # plot as part of comparison several datasets
   if (!is.na(match('NASA', datasets)) & length(datasets) > 1){
-    p <- plot_plural(p, D$val, D$y, m_new, "orange", textNASA_short, out[2], 45) 
+    p <- plot_plural_lines(p, D$val, D$y, m_new, "orange", textNASA_short, out[2], 45) 
     top5(period, D$val, D$y, m_new, textNASA_short) 
   }
 
@@ -165,8 +163,9 @@ globtemp <- function(datasets, refs, refe, period, save_option, save_name){
   if (!is.na(match('Copernicus', datasets)) & length(datasets) == 1){
     # check if dataset covers reference period, plot or print warning
     if (refs >= y1_era5){
-    #  p <- plot_single_points(p, D2$val, D2$y, D2$col, m2_new, textCopernicus) 
-    p <- plot_single_bars(p, D2$val, D2$y, D2$col, m2_new, textCopernicus) 
+      if (plot_type == 'points')  p <- plot_singular_points(p, D2$val, D2$y, D2$col, m2_new, textCopernicus, out2[2]) 
+      if (plot_type == 'bars')    p <- plot_singular_bars(p, D2$val, D2$y, D2$col, m2_new, textCopernicus, out2[2]) 
+      if (plot_type != 'bars' & plot_type != 'points') cat(paste0('** WARNING **: set plot_type to "points" or "bars" \n\n'))
       top5(period, D2$val, D2$y, m2_new, textCopernicus_short) 
     } else {
       p <- p + labs(subtitle = paste(textCopernicus)) + theme(plot.subtitle = element_text(color = "red"))
@@ -176,7 +175,7 @@ globtemp <- function(datasets, refs, refe, period, save_option, save_name){
   if (!is.na(match('Copernicus', datasets)) & length(datasets) > 1){ 
     # check if dataset covers reference period, plot or print warning
     if (refs >= y1_era5){
-      p <- plot_plural(p, D2$val, D2$y, m2_new, "red", textCopernicus_short, out2[2], 5)
+      p <- plot_plural_lines(p, D2$val, D2$y, m2_new, "red", textCopernicus_short, out2[2], 5)
       top5(period, D2$val, D2$y, m2_new, textCopernicus_short) 
     } else {
       p <- p + labs(subtitle = paste(textCopernicus)) + theme(plot.subtitle = element_text(color = "red"))
@@ -186,13 +185,14 @@ globtemp <- function(datasets, refs, refe, period, save_option, save_name){
   # HadCRUT
   # plot as single dataset
   if (!is.na(match('HadCRUT', datasets)) & length(datasets) == 1){
-   # p <- plot_single_points(p, D3$val, D3$y, D3$col, m3_new, textHadCRUT) 
-    p <- plot_single_bars(p, D3$val, D3$y, D3$col, m3_new, textHadCRUT) 
+    if (plot_type == 'points') p <- plot_singular_points(p, D3$val, D3$y, D3$col, m3_new, textHadCRUT, out3[2]) 
+    if (plot_type == 'bars')   p <- plot_singular_bars(p, D3$val, D3$y, D3$col, m3_new, textHadCRUT, out3[2]) 
+    if (plot_type != 'bars' & plot_type != 'points') cat(paste0('** WARNING **: set plot_type to "points" or "bars" \n\n'))
     top5(period, D3$val, D3$y, m3_new, textHadCRUT_short) 
   }
   # plot as part of comparison several datasets
   if (!is.na(match('HadCRUT',datasets)) & length(datasets) > 1){
-    p <- plot_plural(p, D3$val, D3$y, m3_new, "brown", textHadCRUT_short, out3[2], -35) 
+    p <- plot_plural_lines(p, D3$val, D3$y, m3_new, "brown", textHadCRUT_short, out3[2], -35) 
     top5(period, D3$val, D3$y, m3_new, textHadCRUT_short) 
   }
 
